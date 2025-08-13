2ab631625a43870aebf4509f66eb12028c205fa4
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-08-13 20:39:48 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,19 @@
-from tkinter import *
-from tkinter import ttk
+import platform
+from tkinter import Tk, Label, Button, ttk
+
+def callSpecs():
+    cpu_make = platform.processor()
+    return cpu_make
+
 root = Tk()
-root.title("Feet to Meters")
+root.title("Rheolwr-Proses")
+root.geometry("480x640")
+
+
+frame = ttk.Frame(root)
+frame.grid(row=0, column=0)
+
+Label(frame, text=callSpecs()).grid(column=0, row=0)  # label inside frame
+Button(frame, text="Quit", command=root.destroy).grid(column=0, row=1)  # different row
+
+root.mainloop()
