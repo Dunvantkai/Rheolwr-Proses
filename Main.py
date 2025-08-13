765d54dc8ea7d0014e0ffd609ba760072dbf3858
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-08-13 20:51:18 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,21 @@
 import platform
+import tkinter as tk
+ tkinter import ttk
 from tkinter import Tk, Label, Button, ttk
+from tkinter import font
+root = Tk()
+root.title("Rheolwr-Proses")
+root.geometry("580x640")
+
 
 def callSpecs():
     cpu_make = platform.processor()
     return cpu_make
 
-root = Tk()
-root.title("Rheolwr-Proses")
-root.geometry("480x640")
 
+Button(text="CPU").grid(column=1, row=4, padx=20, pady=10)
 
-frame = ttk.Frame(root)
-frame.grid(row=0, column=0)
-
-Label(frame, text=callSpecs()).grid(column=0, row=0)  # label inside frame
-Button(frame, text="Quit", command=root.destroy).grid(column=0, row=1)  # different row
+# Label(frame, text=callSpecs()).grid(column=0, row=0)
+Button(text="Quit", command=root.destroy).grid(column=10, row=10)
 
 root.mainloop()
