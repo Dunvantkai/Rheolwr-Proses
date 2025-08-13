73abaa9237b47c100bd33f4e53e63335dd3534cb
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-08-13 22:27:18 +1200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,75 @@
 import platform
+import wmi
+import GPUtil
+import psutil
+# pip install wmi
+# pip install gputil
+# pip install psutil
+
+from tkinter import *
 import tkinter as tk
- tkinter import ttk
-from tkinter import Tk, Label, Button, ttk
-from tkinter import font
-root = Tk()
+from tkinter import ttk
+
+root = tk.Tk()
 root.title("Rheolwr-Proses")
 root.geometry("580x640")
+root.resizable(width=False, height=False)
 
+style = ttk.Style()
+style.layout("TNotebook", [])
+style.layout("TNotebook.Tab", [])
 
-def callSpecs():
-    cpu_make = platform.processor()
-    return cpu_make
+notebook = ttk.Notebook(root)
+cpuTab = Frame(notebook)
+gpuTab = Frame(notebook)
+ramTab = Frame(notebook)
+notebook.add(cpuTab, text="CPU")
+notebook.add(gpuTab, text="GPU")
+notebook.add(ramTab, text="RAM")
 
+notebook.grid(row=0, column=1)
 
-Button(text="CPU").grid(column=1, row=4, padx=20, pady=10)
+def cpuGetInfo():
+    i = wmi.WMI()
+    for processor in i.Win32_Processor():
+        cpu_make = processor.Name
+        return cpu_make
+    
+def gpuGetInfo():
+    gpus = GPUtil.getGPUs()
+    for gpu in gpus:
+        gpu_make = gpu.name
+        gpu_mem = gpu.memoryTotal
+        return gpu_make + gpu_mem
 
-# Label(frame, text=callSpecs()).grid(column=0, row=0)
-Button(text="Quit", command=root.destroy).grid(column=10, row=10)
+def cpu_page():
+    cpu_label = Label(cpuTab, text="")
+    cpu_label.grid(row=0, column=0)   
+    return cpu_label
+
+def gpu_page():
+    gpu_label = Label(gpuTab, text="")
+    gpu_label.grid(row=0, column=0)   
+    return gpu_label
+
+def show_cpu():
+    cpu_label = cpu_page()
+    cpu_make = cpuGetInfo() 
+    cpu_label.config(text=cpu_make)
+    notebook.select(cpuTab)
+
+def show_gpu():
+    gpu_label = gpu_page()
+    Gpu_make = gpuGetInfo() 
+    gpu_label.config(text=Gpu_make)
+    notebook.select(gpuTab)
+
+def show_ram():
+    notebook.select(ramTab)
+
+Button(root, text="CPU", command=show_cpu, relief="solid", padx=40, pady=10, activebackground="grey", activeforeground="white").grid(column=0, row=0, padx=10, pady=8)
+Button(root, text="GPU", command=show_gpu, relief="solid", padx=40, pady=10, activebackground="grey", activeforeground="white").grid(column=0, row=1, padx=10, pady=8)
+Button(root, text="RAM", command=show_ram, relief="solid", padx=40, pady=10, activebackground="grey", activeforeground="white").grid(column=0, row=2, padx=10, pady=8)
+Button(root, text="Quit", command=root.destroy, relief="solid", padx=40, pady=10, activebackground="grey", activeforeground="white").grid(column=0, row=3, padx=10, pady=8)
 
 root.mainloop()
